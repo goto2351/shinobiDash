f4b5f9876e553adcccc297bee3f850ddb9b787f8
goto2351 <gto9682@yahoo.co.jp>
2022-07-09 17:26:37 +0900

    シーン"AnimationSetting"にアニメーションの遷移とジャンプ処理を追加

---
+++ pazzleGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+
+
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
+    [SerializeField] private float jumpForce;
+
+    private bool isJumping = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // スペースキーでジャンプ
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isJumping = true;
+        }
+
+        // ジャンプ状態を取得し、Animatorと同期する
+        if (gameObject.transform.position.y > 0)
+        {
+            isJumping = true;
+        } else
+        {
+            isJumping = false;
+        }
+        animator.SetBool("isJumping", isJumping);
+    }
+}
