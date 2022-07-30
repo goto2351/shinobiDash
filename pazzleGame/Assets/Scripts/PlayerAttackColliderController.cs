5774b2982948c9b0a2d7453c3f6fa8f5e7f40e77
goto2351 <gto9682@yahoo.co.jp>
2022-07-30 18:00:13 +0900

    攻撃のヒット時に敵を消滅させるようにした

---
+++ pazzleGame/Assets/Scripts/PlayerAttackColliderController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class PlayerAttackColliderController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // “G‚ÉUŒ‚‚ª“–‚½‚Á‚½‚ÌÁ–Åˆ—
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.transform.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
