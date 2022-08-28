37ace1873aa6be9ba550642027424cc87528b6c7
goto2351 <gto9682@yahoo.co.jp>
2022-08-28 15:06:31 +0900

    タイトル画面用のシーンを追加

---
+++ pazzleGame/Assets/Scripts/21_Title/TitleManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : Config
+{
+    //[SerializeField] private GameObject canvas_Title;
+    [SerializeField] private GameObject canvas_HowtoPlay;
+    // スタートボタンが押されたときに本編のシーンを読み込む
+    public void LoadFieldScene()
+    {
+        SceneManager.LoadScene(SCENE_NAME_FIELD);
+    }
+
+    // 「あそびかた」ボタンが押された時遊び方を表示する
+    public void ShowHowtoPlay()
+    {
+        canvas_HowtoPlay.SetActive(true);
+    }
+
+    // 遊び方画面で「もどる」ボタンが押された時遊び方を非表示にする
+    public void HideHowtoPlay()
+    {
+        canvas_HowtoPlay.SetActive(false);
+    }
+}
