b6f5a606b74351eeb0c42ec8bda535e1323bc042
goto2351 <gto9682@yahoo.co.jp>
2022-09-19 19:55:38 +0900

    twitter投稿機能を作成(.jslibはWebGLで実行している時のため)

--- pazzleGame/Assets/Scripts/11_Result/ResultManager.cs
+++ pazzleGame/Assets/Scripts/11_Result/ResultManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,8 +8,27 @@ public class ResultManager : Config
 {
     public GameObject gameOverObj;
 
+    // Twitterリンクを開く関数
+#if !UNITY_EDITOR && UNITY_WEBGL
+    [DllImport("__Internal")]
+    private static extern void OpenNewTab(string url);
+#else
+    private static void OpenNewTab(string url) => Application.OpenURL(url);
+#endif
+
+    // リトライボタンを押したときに本編のシーンをロードする
     public void LoadNextScene()
     {
         SceneManager.LoadScene(SCENE_NAME_FIELD);
+    }
+
+    // 「結果をツイート」ボタンを押したときにTwitter投稿リンクを開く
+    public void OpenTweetSubmitForm()
+    {
+        string tweetText = "ツイートテスト 結果: " + current_distance + " km";
+        var URL = new System.Uri("https://twitter.com/intent/tweet?text=" + tweetText);
+        //Debug.Log(URL.AbsoluteUri);
+        OpenNewTab(URL.AbsoluteUri);
+
     }
 }
